09fdd79e4bafab2215bbae6f60e1b10ba3718448
Tinex3 <benjamin.riquelmego@usm.cl>
2023-09-21 23:06:46 -0300

    53w5

---
+++ Sensado de informacion y subir a webserver/build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -0,0 +1,12 @@
+# 1 "E:\\GitHub\\Micro Arduino\\EIN_246-Automatizacion-y-control-\\Sensado de informacion y subir a webserver\\Sensado de informacion y subir a webserver.ino"
+
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
---
+++ Sensado de informacion y subir a webserver/build/sketch/Sensado de informacion y subir a webserver.ino.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#line 1 "E:\\GitHub\\Micro Arduino\\EIN_246-Automatizacion-y-control-\\Sensado de informacion y subir a webserver\\Sensado de informacion y subir a webserver.ino"
+
+
+#line 3 "E:\\GitHub\\Micro Arduino\\EIN_246-Automatizacion-y-control-\\Sensado de informacion y subir a webserver\\Sensado de informacion y subir a webserver.ino"
+void setup();
+#line 8 "E:\\GitHub\\Micro Arduino\\EIN_246-Automatizacion-y-control-\\Sensado de informacion y subir a webserver\\Sensado de informacion y subir a webserver.ino"
+void loop();
+#line 3 "E:\\GitHub\\Micro Arduino\\EIN_246-Automatizacion-y-control-\\Sensado de informacion y subir a webserver\\Sensado de informacion y subir a webserver.ino"
+void setup()
+{
+	
+}
+
+void loop()
+{
+	
+}
+
